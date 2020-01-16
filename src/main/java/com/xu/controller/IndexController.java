f8ef9e97d08e2636474390ff57eb6ec399f97a7b
xuhang <320853112@qq.com>
2020-01-16 17:38:09 +0800

    1.16

---
+++ src/main/java/com/xu/controller/IndexController.java
@@ -0,0 +1,9 @@
+package com.xu.controller;
+
+import com.xu.annotation.XuController;
+import com.xu.annotation.XuRequestMapping;
+
+@XuController
+@XuRequestMapping("/test")
+public class IndexController {
+}
--- src/main/java/com/xu/springMVC/servlet/XuDispatcherServlet.java
+++ src/main/java/com/xu/springMVC/servlet/XuDispatcherServlet.java
@@ -1,7 +1,17 @@
 package com.xu.springMVC.servlet;
 
+import com.xu.utils.ClassUtil;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class XuDispatcherServlet extends HttpServlet {
 
+
+    @Override
+    public void init() throws ServletException {
+        //获取当前包下所有的类
+        ClassUtil.getClasses("com.xu.controller");
+        //判断类上是否有XuController的注解
+    }
 }
