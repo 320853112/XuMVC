5524b76df54bd79b4bc48335a9965efcbc700b2e
xuhang <320853112@qq.com>
2020-01-17 16:05:31 +0800

    1.17

--- src/main/java/com/xu/springMVC/servlet/XuDispatcherServlet.java
+++ src/main/java/com/xu/springMVC/servlet/XuDispatcherServlet.java
@@ -7,6 +7,9 @@ import com.xu.utils.MyClassUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -20,8 +23,8 @@ public class XuDispatcherServlet extends HttpServlet {
     private ConcurrentHashMap<String,Object> springMVCBeans = new ConcurrentHashMap<String, Object>();
     //（url,类名）
     private ConcurrentHashMap<String,Object> urlBeans = new ConcurrentHashMap<String, Object>();
-    //（url,方法）
-    private ConcurrentHashMap<String,Object> urlMethod = new ConcurrentHashMap<String, Object>();
+    //（url,方法名）
+    private ConcurrentHashMap<String,String> urlMethod = new ConcurrentHashMap<String, String>();
 
 
     @Override
@@ -37,6 +40,44 @@ public class XuDispatcherServlet extends HttpServlet {
         }
         //将url映射出去
         handlerMapping();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //处理请求
+        //获取请求url
+        String requestURL = req.getRequestURI();
+        if (requestURL==null) {
+            return;
+        }
+        Object object = urlBeans.get(requestURL);
+        if(object==null){
+            resp.getWriter().write("page not find 404");
+            return;
+        }
+        String methodName = urlMethod.get(requestURL);
+        if(methodName==null){
+            resp.getWriter().write("method not find");
+            return;
+        }
+        String resultPage = (String)methodInvoke(object, methodName);
+        resp.getWriter().write(resultPage);
+
+    }
+
+    private Object methodInvoke(Object object,String methodName){
+        try {
+            Method method = object.getClass().getMethod(methodName);
+            Object result = method.invoke(object);
+            return result;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public void findClassMVCAnnotation(List<Class> classes) throws IllegalAccessException, InstantiationException {
